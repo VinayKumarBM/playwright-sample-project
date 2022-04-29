6e2d5c3d3736e61c4ff6486ec078ebec4d4f7032
VinayKumarBM <bmvinayk@gmail.com>
2022-04-29 18:54:14 +0530

    Add JUnit HTML reporter

--- src/framework/constants/CommonConstants.ts
+++ src/framework/constants/CommonConstants.ts
@@ -12,6 +12,9 @@ export default class CommonConstants {
     static readonly REST_JSON_REQUEST_PATH = "src/resources/API/REST/";
     static readonly TEST_FOLDER_PATH = "../../tests/";
     static readonly TEST_SUITE_FILE_FORMAT = ".test.ts";
-    static readonly PARALLEL_MODE: "parallel";
-    static readonly SERIAL_MODE: "serial";
+    static readonly PARALLEL_MODE = "parallel";
+    static readonly SERIAL_MODE = "serial";
+    static readonly REPORT_TITLE = "Test Execution Report";
+    static readonly RESULTS_PATH = "./test-results/results";
+    static readonly JUNIT_RESULTS_PATH = `${CommonConstants.RESULTS_PATH}/results.xml`;
 }
