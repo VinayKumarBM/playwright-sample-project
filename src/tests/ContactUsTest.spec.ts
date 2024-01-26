a4d50ecce7133344e46f82cd42e35c443f0056f7
mcarolinasouza <m_carolinasouza@hotmail.com>
2024-01-26 20:16:18 -0300

    Updated file

--- src/tests/ContactUsTest.spec.ts
+++ src/tests/ContactUsTest.spec.ts
@@ -6,7 +6,7 @@ import HomeSteps from "@uiSteps/HomeSteps";
 const testData = ExcelUtil.getTestDataArray("ContactUsTest");
 // eslint-disable-next-line no-restricted-syntax
 for (const data of testData) {
-    test(`${data.TestID} - ${data.Description}`, async ({ page }) => {
+    test(`${data.TestID} : ${data.Description}`, async ({ page }) => {
         Allure.attachDetails(data.Description, data.Issue);
         const home = new HomeSteps(page);
         await home.launchApplication();
